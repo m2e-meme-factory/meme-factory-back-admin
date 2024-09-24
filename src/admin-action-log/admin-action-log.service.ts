6064149415231704d623a323ba797bdca9628c13
K0rzet <ehot2004@gmail.com>
2024-09-24 22:03:28 +0300

    added get all admin logs endpoint

--- src/admin-action-log/admin-action-log.service.ts
+++ src/admin-action-log/admin-action-log.service.ts
@@ -1,21 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { Observer } from 'src/observer/observer.interface';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { Observer } from 'src/observer/observer.interface'
+import { AdminActionLog } from '@prisma/client'
 
 @Injectable()
 export class AdminActionLogService implements Observer {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
 
-  async update(action: string, details: any): Promise<void> {
-    await this.prisma.adminActionLog.create({
-      data: {
-        action: details.action,
-        entityType: details.entityType,
-        entityId: details.entityId,
-        oldData: details.oldData,
-        newData: details.newData,
-        adminId: details.adminId,
-      },
-    });
-  }
+	async update(action: string, details: any): Promise<void> {
+		await this.prisma.adminActionLog.create({
+			data: {
+				action: details.action,
+				entityType: details.entityType,
+				entityId: details.entityId,
+				oldData: details.oldData,
+				newData: details.newData,
+				adminId: details.adminId
+			}
+		})
+	}
+
+	async getAll(): Promise<AdminActionLog[]> {
+		return await this.prisma.adminActionLog.findMany()
+	}
 }
